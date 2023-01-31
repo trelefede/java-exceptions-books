5826256875e43aef6e0d88b371fc144a7e8b0506
TreleaniFederico <treleani.federico@gmail.com>
2023-01-31 16:06:50 +0100

    add exception to getter and setters + add try catch to new Book object

--- src/org/lessons/java/exceptions/books/Book.java
+++ src/org/lessons/java/exceptions/books/Book.java
@@ -7,18 +7,35 @@ public class Book {
 	private String autore;
 	private String editore;
 
-	public Book(String titolo, int nPagine, String autore, String editore) {
+	// constructor
+	public Book(String titolo, int nPagine, String autore, String editore) throws Exception {
+
+		if (titolo.isEmpty())
+			throw new Exception("Titolo obbligatorio!");
 		this.titolo = titolo;
+
+		if (nPagine <= 0)
+			throw new Exception("Pagine deve essere maggiore di zero!");
 		this.nPagine = nPagine;
+
+		if (autore.isEmpty())
+			throw new Exception("Autore obbligatorio!");
 		this.autore = autore;
+
+		if (editore.isEmpty())
+			throw new Exception("Editore obbligatorio!");
 		this.editore = editore;
 	}
 
+	// getter and setter
 	public String getTitolo() {
 		return titolo;
 	}
 
-	public void setTitolo(String titolo) {
+	public void setTitolo(String titolo) throws Exception {
+		if (titolo.isEmpty()) {
+			throw new Exception("Titolo obbligatorio");
+		}
 		this.titolo = titolo;
 	}
 
@@ -26,24 +43,42 @@ public class Book {
 		return nPagine;
 	}
 
-	public void setnPagine(int nPagine) {
-		this.nPagine = nPagine;
+	public void setnPagine(int nPagine) throws Exception {
+		if (nPagine <= 0) {
+			throw new Exception("Pagine deve essere maggiore di zero!");
+		} else {
+			this.nPagine = nPagine;
+		}
 	}
 
 	public String getAutore() {
 		return autore;
 	}
 
-	public void setAutore(String autore) {
-		this.autore = autore;
+	public void setAutore(String autore) throws Exception {
+		if (autore.isEmpty()) {
+			throw new Exception("Autore obbligatorio!");
+		} else {
+			this.autore = autore;
+		}
 	}
 
 	public String getEditore() {
 		return editore;
 	}
 
-	public void setEditore(String editore) {
-		this.editore = editore;
+	public void setEditore(String editore) throws Exception {
+		if (editore.isEmpty()) {
+			throw new Exception("Editore obbligatorio!");
+		} else {
+			this.editore = editore;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "\n" + "Titolo: " + getTitolo() + "\n" + "Autore: " + getAutore() + "\n" + "Editore: " + getEditore()
+				+ "\n" + "Pagine: " + getnPagine() + "\n" + "\n";
 	}
 
 }
--- src/org/lessons/java/exceptions/books/Catalogo.java
+++ src/org/lessons/java/exceptions/books/Catalogo.java
@@ -1,5 +1,46 @@
 package org.lessons.java.exceptions.books;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Catalogo {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quanti libri vuoi aggiugere? ");
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		Book[] catalogo = new Book[n];
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("Libro numero: " + (i + 1));
+			System.out.println("Inserisci titolo: ");
+			String titolo = sc.nextLine();
+
+			System.out.println("Inserisci autore: ");
+			String autore = sc.nextLine();
+
+			System.out.println("Inserisci editore: ");
+			String editore = sc.nextLine();
+
+			System.out.println("Inserisci numero pagine: ");
+			int pagine = sc.nextInt();
+			sc.nextLine();
+
+			try {
+				catalogo[i] = new Book(titolo, pagine, autore, editore);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println(Arrays.toString(catalogo));
+
+		sc.close();
+
+	}
 }
